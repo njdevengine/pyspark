1e0d6d15f7ec5033923c2cb2628f2b31528b3dd8
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-08-27 03:00:17 -0400

    Update sqlalchemy_pandas.py

--- sqlalchemy_pandas.py
+++ sqlalchemy_pandas.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-file = '/path/to/csv/file'
+file = 'myfile_or_filepath.csv'
 
 #preview the file
-pd.read_csv(file, nrows=5)
+# sample = pd.read_csv(file, nrows=20)
 
 csv_database = create_engine('sqlite:///csv_database.db')
 
@@ -13,10 +13,26 @@ chunksize = 100000
 i = 0
 j = 1
 for df in pd.read_csv(file, chunksize=chunksize, iterator=True):
-      df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) 
-      df.index += j
-      i+=1
-      df.to_sql('table', csv_database, if_exists='append')
-      j = df.index[-1] + 1
-#dont really want to load the whole db, change select statement below      
-df = pd.read_sql_query('SELECT * FROM table', csv_database)
+    df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) 
+    df.index += j
+    i+=1
+    df.to_sql('table', csv_database, if_exists='append')
+    j = df.index[-1] + 1
+
+
+query = """
+SELECT * FROM "table" WHERE "Unnamed:0"='id';
+"""
+df = pd.read_sql_query(query, csv_database)
+
+#gets columns
+#SELECT sql FROM sqlite_master WHERE name='table';
+#df.sql[0]
+
+#gets connection url
+#csv_database.url
+
+#also gets table names
+#csv_database.table_names()
+
+
